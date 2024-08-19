62de9d3f5eee267b5e5d09cc2c8efcd33aadb28b
endomorphosis <starworks5@gmail.com>
2024-08-19 02:15:57 -0700

    refactor: Update imports in index.js to use ipfs_model_manager.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-export * from '/ipfs_model_manager_js/model_manager.js';
-export * from '/ipfs_model_manager_js/s3.js';
+export * from '/ipfs_model_manager_js/ipfs_model_manager.js';
 export * from '/ipfs_model_manager_js/s3_kit.js';
 export * from '/ipfs_model_manager_js/test_fio.js';
