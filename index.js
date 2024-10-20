86280675360d08ed14f9e7997d8e4f19a2dce664
endomorphosis <starworks5@gmail.com>
2024-10-20 00:21:39 -0700

    refactor: Update imports and class names in test.js

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ export { ipfsModelManagerJs } from './ipfs_model_manager_js/ipfs_model_manager.j
 export { ipfsModelManagerJs as default } from './ipfs_model_manager_js/ipfs_model_manager.js';
 export * from './test/test.js'
 export { test_ipfs_model_manager_js } from './test/test.js'
+import { test_ipfs_model_manager_js } from './test/test.js'
 const test = new test_ipfs_model_manager_js();
 export { test } 
